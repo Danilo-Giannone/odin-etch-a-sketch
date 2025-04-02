3951a9f1fc7bde2c1bcf7d76140265ce203278bc
Danilo Giannone <danilogiannone00@gmail.com>
2025-04-02 19:22:14 +0200

    fix a problem with the view of the grid, now it's always visible in it's entirety.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,18 @@
 let grid = document.querySelector("#grid");
-let dimension = 16;
+let dimension = 10;
+window.onload = refresh();
+window.addEventListener('resize', function(event){
+    refresh();
+  });
+
+function refresh(){
+    if(window.innerWidth > window.innerHeight){
+        grid.setAttribute("style", "height:70vh; width: 70vh")
+    }else{
+        grid.setAttribute("style", "height:70vw; width: 70vw")
+    }
+};
+
 
 for(let i = 0; i < dimension; i++){
     let row = document.createElement("div");
@@ -9,8 +22,15 @@ for(let i = 0; i < dimension; i++){
     for (let j = 0; j < dimension; j++) {
         let square = document.createElement("div");
         square.classList.add("square");
-        square.id = i.toString() + j.toString();
+        square.id = i.toString() + "_" + j.toString();
         row.appendChild(square);
         
     }
 }
+let squares = document.querySelectorAll(".square");
+squares.forEach(square => {
+    square.addEventListener("mouseover", () => {
+        console.log(square.id);
+        
+    })
+});
